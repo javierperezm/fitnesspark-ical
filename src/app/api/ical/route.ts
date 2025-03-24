bf233dbe7fdea5560ee48ac3ec49f6451919d876
Javier Pérez <hi@javierperez.com>
2025-03-24 12:28:20 +0100

    fix timezone

--- src/app/api/ical/route.ts
+++ src/app/api/ical/route.ts
@@ -42,7 +42,7 @@ export const GET = async (req: Request) => {
 
     calendar.createEvent({
       start: fullDate,
-      // timezone: 'Europe/Zurich',
+      timezone: 'Europe/Zurich',
       end: new Date(fullDate.getTime() + event.duration * 60000),
       summary: `${event.name} - ${event.trainer}`,
       description: `Room: ${event.room}, Status: ${event.status}, Free Slots: ${event.freeSlots}, Trainer: ${event.trainer}`,
